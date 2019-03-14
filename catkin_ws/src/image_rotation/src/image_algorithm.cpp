a316314b99048924e63721c82d10a4fa5eb8bbb0
upengareri <u.gareri@gmail.com>
2019-03-14 16:09:16 +0100

    make review comments fixed

--- catkin_ws/src/image_rotation/src/image_algorithm.cpp
+++ catkin_ws/src/image_rotation/src/image_algorithm.cpp
@@ -1,9 +1,9 @@
 ///
 /// @file
-/// @brief Implementation of image processing class
+/// @brief Implementation of image processing algorithm
 ///
 
-#include "ImageAlgorithm.h"
+#include "image_algorithm.h"
 
 using namespace cv;
 
@@ -13,11 +13,11 @@ void ImageAlgorithm::RotateImage(Mat &image, double angle)
     Point2f center((image.cols - 1) / 2.0, (image.rows - 1) / 2.0);
     Mat rotation_matrix = getRotationMatrix2D(center, angle, 1.0);
     // determine bounding rectangle, center not relevant
-    Rect2f bbox = RotatedRect(Point2f(), image.size(), angle).boundingRect2f();
+    Rect2f bounding_box = RotatedRect(Point2f(), image.size(), angle).boundingRect2f();
     // adjust transformation matrix
-    rotation_matrix.at<double>(0, 2) += bbox.width / 2.0 - image.cols / 2.0;
-    rotation_matrix.at<double>(1, 2) += bbox.height / 2.0 - image.rows / 2.0;
+    rotation_matrix.at<double>(0, 2) += bounding_box.width / 2.0 - image.cols / 2.0;
+    rotation_matrix.at<double>(1, 2) += bounding_box.height / 2.0 - image.rows / 2.0;
 
-    warpAffine(image, image, rotation_matrix, bbox.size());
+    warpAffine(image, image, rotation_matrix, bounding_box.size());
 }
 
