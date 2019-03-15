9566a6575c30e1f40412d07eb617d7d2b27ffa02
ash <aswinkumar.work@gmail.com>
2019-03-15 10:49:08 +0100

    Item_2_Sprint_1911/AV: Modified Subscriber File to use openCV and corresponding CMake Changes

--- catkin_ws/src/image_rotation/src/image_rotation_subscriber.cpp
+++ catkin_ws/src/image_rotation/src/image_rotation_subscriber.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 #include <ros/ros.h>
+#include "../include/utilities.hpp"
 
-int main(int argc, char* argv[])
+
+#include <image_transport/image_transport.h>
+#include "std_msgs/String.h"
+#include "sensor_msgs/Image.h"
+#include <sensor_msgs/image_encodings.h>
+
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+static const std::string OPENCV_WINDOW = "Image Window";
+
+void ImageRotationCallback(const sensor_msgs::ImageConstPtr& msg)
 {
-   ros::init(argc, argv, "ImageSubscriberNode");
-   ros::NodeHandle image_subscriber_handle; 
+    utilities::helloworld();
+    ROS_INFO("%d x %d",msg->width,msg->height);
+    cv::namedWindow(OPENCV_WINDOW);
+    cv_bridge::CvImagePtr cv_ptr;
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    
+
+    cv::imshow(OPENCV_WINDOW,cv_ptr->image);
+
+    cv::waitKey(3);
+}
+
+int  main(int argc, char* argv[])
+{ 
+    ros::init(argc, argv, "ImageSubscriberNode");
+    ros::NodeHandle image_subscriber_handle; 
+    ros::Subscriber image_subscriber = image_subscriber_handle.subscribe("/sensors/camera/image_color",1000,ImageRotationCallback);
+    ros::spin();
     return 0;
 }
