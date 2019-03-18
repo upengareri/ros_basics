dfa45c8aca44f3e3273191d9e3b90942ce4c2803
ash <aswinkumar.work@gmail.com>
2019-03-18 11:42:43 +0100

    Item_2_Sprint_1911/AV: Review Comment Fixes and Changes in CMake to remove relative path usage in the code

--- catkin_ws/src/image_rotation/src/image_rotation_subscriber.cpp
+++ catkin_ws/src/image_rotation/src/image_rotation_subscriber.cpp
@@ -4,18 +4,10 @@
 ///
 
 #include <iostream>
-
-#include "../include/image_rotation/image_algorithm.h"
-
+#include "image_algorithm.h"
 #include <cv_bridge/cv_bridge.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
 #include <image_transport/image_transport.h>
 #include <ros/ros.h>
-#include "sensor_msgs/Image.h"
-#include <sensor_msgs/image_encodings.h>
-#include "std_msgs/String.h"
 
 static const std::string OPENCV_WINDOW = "Image Window";
 
@@ -34,7 +26,8 @@ int  main(int argc, char* argv[])
 { 
     ros::init(argc, argv, "ImageSubscriberNode");
     ros::NodeHandle image_subscriber_handle; 
-    ros::Subscriber image_subscriber = image_subscriber_handle.
+    image_transport::ImageTransport image_transport_handle(image_subscriber_handle);
+    image_transport::Subscriber image_subscriber = image_transport_handle.
                                                   subscribe("/sensors/camera/image_color",1000,ImageRotationCallback);
     ros::spin();
     return 0;
