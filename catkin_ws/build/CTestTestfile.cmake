284e868b0663f837dd04ed84eaace98559ea44b7
ash <aswinkumar.work@gmail.com>
2019-03-18 17:03:28 +0100

    Item_2_Sprint_1911/AV: Removal of build folder contents

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ash/BMW-Sprints/LearningSprint/ImageRotation/ros_basics/catkin_ws/src
-# Build directory: /home/ash/BMW-Sprints/LearningSprint/ImageRotation/ros_basics/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- catkin_ws/build/image_rotation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ash/BMW-Sprints/LearningSprint/ImageRotation/ros_basics/catkin_ws/src/image_rotation
-# Build directory: /home/ash/BMW-Sprints/LearningSprint/ImageRotation/ros_basics/catkin_ws/build/image_rotation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
