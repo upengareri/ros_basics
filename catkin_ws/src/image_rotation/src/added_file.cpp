92df1edc0e57f48de4ce4ea34f482e342c173c0e
melisa <Melisa.Jose@partner.bmw.de>
2019-07-26 14:34:29 +0200

    Check with removed file contents

--- catkin_ws/src/image_rotation/src/added_file.cpp
+++ catkin_ws/src/image_rotation/src/added_file.cpp
@@ -9,10 +9,7 @@ using namespace cv;
 
 void ImageAlgorithm::RotateImage(Mat &image, double angle)
 {
-    // get rotation matrix for rotating the image around its center in pixel coordinates
-    Point2f center((image.cols - 1) / 2.0, (image.rows - 1) / 2.0);
-    Mat rotation_matrix = getRotationMatrix2D(center, angle, 1.0);
-    // determine bounding rectangle, center not relevant
+    
     Rect2f bounding_box = RotatedRect(Point2f(), image.size(), angle).boundingRect2f();
     // adjust transformation matrix
     rotation_matrix.at<double>(0, 2) += bounding_box.width / 2.0 - image.cols / 2.0;
