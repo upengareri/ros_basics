719ce7e569c093e6caf708300a989ceb65e63d54
melisa <Melisa.Jose@partner.bmw.de>
2019-07-26 14:23:40 +0200

    Add repeated lines of code

--- catkin_ws/src/image_rotation/src/added_file.cpp
+++ catkin_ws/src/image_rotation/src/added_file.cpp
@@ -18,6 +18,12 @@ void ImageAlgorithm::RotateImage(Mat &image, double angle)
     rotation_matrix.at<double>(0, 2) += bounding_box.width / 2.0 - image.cols / 2.0;
     rotation_matrix.at<double>(1, 2) += bounding_box.height / 2.0 - image.rows / 2.0;
 
+    Rect2f bounding_box = RotatedRect(Point2f(), image.size(), angle).boundingRect2f();
+    // adjust transformation matrix
+    rotation_matrix.at<double>(0, 2) += bounding_box.width / 2.0 - image.cols / 2.0;
+    rotation_matrix.at<double>(1, 2) += bounding_box.height / 2.0 - image.rows / 2.0;
+
+
     warpAffine(image, image, rotation_matrix, bounding_box.size());
 }
 
